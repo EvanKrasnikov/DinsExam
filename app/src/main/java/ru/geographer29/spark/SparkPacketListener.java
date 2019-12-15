7de329aecbeab31fd6442089fb37bdd1834fd10a
Evan Krasnikov <geographer29@yandex.ru>
2019-12-15 23:49:04 +0300

    Add condition to notify Kafka

--- app/src/main/java/ru/geographer29/spark/SparkPacketListener.java
+++ app/src/main/java/ru/geographer29/spark/SparkPacketListener.java
@@ -2,13 +2,36 @@ package ru.geographer29.spark;
 
 import org.pcap4j.core.PacketListener;
 import org.pcap4j.packet.Packet;
+import ru.geographer29.kafka.KafkaHandler;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SparkPacketListener implements PacketListener {
-    static int i = 0;
+    private AtomicInteger trafficAmount = new AtomicInteger(0);
+    private final int limitMin = 1024;
+    private final int limitMax = Integer.MAX_VALUE / 2;
+
+    private ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+    private Runnable checkIfLimitsExceed = new Runnable() {
+        @Override
+        public void run() {
+            if (trafficAmount.get() < limitMin || trafficAmount.get() > limitMax){
+                KafkaHandler.notifyKafka();
+                trafficAmount.set(0);
+            }
+        }
+    };
+
+    {
+        executorService.scheduleAtFixedRate(checkIfLimitsExceed, 5, 5, TimeUnit.MINUTES);
+    }
 
     @Override
     public void gotPacket(Packet packet) {
-        //System.out.println("[" + Thread.currentThread().getName() + "] Package length = " + packet.length() + " bytes. Total number of packets = " + ++i);
+        trafficAmount.getAndAdd(packet.length());
     }
 
 }

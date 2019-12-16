29776f09571a03aa84b5cf40bcca2db7572d7603
Evan Krasnikov <geographer29@yandex.ru>
2019-12-17 01:01:01 +0300

    Fixed database connection issues

--- app/src/main/java/ru/geographer29/storage/DBHandler.java
+++ app/src/main/java/ru/geographer29/storage/DBHandler.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.sql.*;
+import java.util.Properties;
 
 public class DBHandler {
     private final static Logger logger = LogManager.getLogger(DBHandler.class);
@@ -15,10 +16,14 @@ public class DBHandler {
     }
 
     public void connect(String host, int port){
-        String url = "jdbc:postgresql://" + host + ":" + port + "/traffic_limits";
-        Connection connection = null;
+        String url = "jdbc:postgresql://" + host + ":" + port + "/";
+        logger.error(url);
+        Properties props = new Properties();
+        props.putIfAbsent("user", "postgres");
+        props.putIfAbsent("password", "password");
+
         try {
-            connection = DriverManager.getConnection(url);
+            connection = DriverManager.getConnection(url, props);
         } catch (SQLException e) {
             logger.error("Unable to connect to database");
             e.printStackTrace();
@@ -40,7 +45,9 @@ public class DBHandler {
                 Statement statement = connection.createStatement();
                 ResultSet resultSet = statement.executeQuery(query);
         ){
-            str = resultSet.getString(1);
+            while (resultSet.next()){
+                str = resultSet.getString(1);
+            }
         } catch (SQLException e) {
             logger.error("Unable to select element");
             e.printStackTrace();

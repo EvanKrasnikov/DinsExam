d67c88f3a6bcb4106b7f75e327064763cc0a2688
Evan Krasnikov <geographer29@yandex.ru>
2019-12-16 03:10:22 +0300

    Queries were changed

--- app/src/main/java/ru/geographer29/storage/Query.java
+++ app/src/main/java/ru/geographer29/storage/Query.java
@@ -2,10 +2,10 @@ package ru.geographer29.storage;
 
 public enum Query {
 
-    SELECT_MIN_TRAFFIC_LIMIT("SELECT limit_value WHERE limit_name = min"),
-    SELECT_MAX_TRAFFIC_LIMIT("SELECT limit_value WHERE limit_name = max"),
-    UPDATE_MIN_TRAFFIC_LIMIT("UPDATE limit_value WHERE limit_name = min"),
-    UPDATE_MAX_TRAFFIC_LIMIT("UPDATE limit_value WHERE limit_name = max");
+    SELECT_MIN_TRAFFIC_LIMIT("SELECT traffic_limits.limit_value WHERE traffic_limits.limit_name = min"),
+    SELECT_MAX_TRAFFIC_LIMIT("SELECT traffic_limits.limit_value WHERE traffic_limits.limit_name = max"),
+    UPDATE_MIN_TRAFFIC_LIMIT("UPDATE traffic_limits.limit_value WHERE traffic_limits.limit_name = min"),
+    UPDATE_MAX_TRAFFIC_LIMIT("UPDATE traffic_limits.limit_value WHERE traffic_limits.limit_name = max");
 
     private String query;
 

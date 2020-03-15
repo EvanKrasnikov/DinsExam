04eabdfea1e1f430641388f946cca3ec78de8c4e
Evan Krasnikov <geographer29@yandex.ru>
2020-03-15 17:38:48 +0300

    DBHandler works with Property.class

--- app/src/main/java/ru/geographer29/storage/DBHandler.java
+++ app/src/main/java/ru/geographer29/storage/DBHandler.java
@@ -6,35 +6,35 @@ import org.apache.logging.log4j.Logger;
 import java.sql.*;
 import java.util.Properties;
 
-public class DBHandler {
+import static ru.geographer29.settings.AppSettings.*;
+
+public class DBHandler implements AutoCloseable{
     private final static Logger logger = LogManager.getLogger(DBHandler.class);
     private Connection connection;
 
-    public void connect(String host, int port){
+    public DBHandler() {
+        Properties props = System.getProperties();
+        String host = props.getProperty(DATABASE_HOST);
+        String port = props.getProperty(DATABASE_PORT);
+        String user = props.getProperty(DATABASE_USER);
+        String password = props.getProperty(DATABASE_PASS);
         String url = "jdbc:postgresql://" + host + ":" + port + "/";
-        Properties props = new Properties();
-        props.putIfAbsent("user", "postgres");
-        props.putIfAbsent("password", "password");
 
         try {
-            connection = DriverManager.getConnection(url, props);
+            connection = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
             logger.error("Unable to connect to database");
             e.printStackTrace();
         }
     }
 
-    public void disconnect(){
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            logger.error("Unable to close connection");
-            e.printStackTrace();
-        }
+    @Override
+    public void close() throws Exception {
+        connection.close();
     }
 
     public String selectElement(String query){
-        String str = null;
+        String str = "";
         try (
                 Statement statement = connection.createStatement();
                 ResultSet resultSet = statement.executeQuery(query);
